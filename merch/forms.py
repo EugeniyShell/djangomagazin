0109bf5d9cfa8b9e15b2ede71d88177cf7bda2b7
Jorian <jorian@mail.ru>
2022-11-23 20:55:05 +0300

    form classes fix

--- merch/forms.py
+++ merch/forms.py
@@ -10,21 +10,11 @@ class ShopUserLoginForm(AuthenticationForm):
         model = ShopUser
         fields = ('username', 'password')
 
-    def __init__(self, *args, **kwargs):
-        super(ShopUserLoginForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
-
 
 class ShopUserRegisterForm(UserCreationForm):
     class Meta:
         model = ShopUser
         fields = ('username', 'password1', 'password2', 'email')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
 
 
 class ShopUserEditForm(UserChangeForm):
@@ -35,7 +25,6 @@ class ShopUserEditForm(UserChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
-            field.help_text = ''
             if field_name == 'password':
                 field.widget = HiddenInput()
+
